3d585cfdc3e3f4f756f64acdb2f08677867fc25a
Nathan Feliphe <nathanfcrocha@gmail.com>
2024-08-06 12:09:42 -0300

    35. Search Insert Position

---
+++ 35.java
@@ -0,0 +1,19 @@
+class searchInsert {
+    public int searchInsert(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++){
+
+            if(nums[i] == target){
+                return i;
+            }
+
+            if(nums[i] > target){
+                return i;
+            }
+
+            if(nums[nums.length -1] < target){
+                return nums.length;
+            }
+        }
+        return target;
+    }
+}
